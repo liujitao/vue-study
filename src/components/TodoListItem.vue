54050a6edcfd58e5e06cf4853f68cad6359543d4
liujitao <liujitao79@gmail.com>
2020-12-11 15:20:23 +0800

    example:todolist

---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="item">
+    <p>
+      <span v-show="!isActive" v-on:click="activeItem">{{ item }}</span>
+      <span v-show="isActive">
+        <input v-model="content" v-on:blur="inactiveItem" type="text" />
+      </span>
+      <span v-on:click="$emit('deleteItem', index)">X</span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoListItem',
+  props: ['item', 'index'],
+  data() {
+    return {
+      content: '',
+      isactive: false,
+    }
+  },
+  methods: {
+    activeItem() {
+      this.isactive = true
+      this.content = this.item
+    },
+    inactiveItem() {
+      this.$emit('modifyItem', this.content, this.index)
+      this.isactive = false
+    },
+  },
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld />
+    <TodoList />
   </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
+import TodoList from '../components/TodoList.vue'
+import '../assets/style.css'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    TodoList,
   },
 }
 </script>
