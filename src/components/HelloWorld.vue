b3bfc13c796d9ef0c27b7fdf6c79fa39fdaaccbc
liujitao <liujitao79@gmail.com>
2020-12-11 14:04:45 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <input v-model="words" type="text" />
+    <h3>{{ words }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      words: 'hello world',
+    }
+  },
+}
+</script>
+
+<style scoped>
+.home {
+  margin: 0 auto;
+  text-align: center;
+  font-size: 20px;
+}
+</style>
